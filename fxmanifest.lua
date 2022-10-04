d2f9dd815650fea2c2ee8856164a4c5bdf14c709
Lucas <genevasterm@protonmail.ch>
2022-10-04 14:54:36 -0500

    style(manifest): that's not the name!

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 game 'gta5'
 
-name 'geneva-baseball'
+name 'geneva-hunting'
 description 'A standalone hunting script for FiveM.'
 author 'geneva/traditionalism'
 repository 'https://github.com/traditionalism/geneva-hunting'
